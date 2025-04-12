514d4451f10ba2b1dcb7af39add67371c8d8ba7e
NapKim Math <napkimmath@gmail.com>
2025-04-12 12:56:49 -0400

    Create create_table_with_data_struct.sql

---
+++ creating_data/uncommented/create_table_with_data_struct.sql
@@ -0,0 +1,46 @@
+/* 
+This code inserts two rows into your table as you're creating it, using STRUCT to bring in each record.
+This took slightly less slot time (609 MS) than using UNION ALL (669 MS).
+https://github.com/napkimmath/building-blocks/blob/main/creating_data/create_table_with_data_union.sql
+
+CAUTION: If you run this more than 60 days after 2025-03-01 and you're using a BigQuery sandbox, 
+your partitions may already be expired.
+
+Click the SQL tab in the first link to see the starter code. Both links provide documentation:
+https://cloud.google.com/bigquery/docs/tables#create_an_empty_table_with_a_schema_definition
+https://cloud.google.com/bigquery/docs/reference/standard-sql/data-definition-language#create_table_statement
+*/
+CREATE OR REPLACE TABLE
+  project.dataset.new_table 
+  (integer_field INT64 OPTIONS (description = 'An INTEGER field'),
+    array_field STRUCT < a STRING OPTIONS (description = 'A STRING field within an ARRAY'),
+    b BOOL OPTIONS (description = 'A BOOLEAN field within an ARRAY') >,
+    string_field string OPTIONS (description = 'A STRING field'),
+    date_field date OPTIONS (description = 'A DATE field'),
+    datetime_field datetime OPTIONS (description = 'A DATETIME field'),
+    boolean_field boolean OPTIONS (description = 'A BOOLEAN field') )
+PARTITION BY
+  date_field 
+OPTIONS ( expiration_timestamp = TIMESTAMP '2026-01-01 00:00:00 UTC',
+    description = 'a table that expires in 2026',
+    labels = [('test_table',
+      'development'),
+    ('empty',
+      'testing')]) 
+  AS
+SELECT
+  *
+FROM
+  UNNEST([ STRUCT( 1 AS integer_field,
+      STRUCT('sample1' AS a,
+        TRUE AS b) AS array_field,
+      'Sample String 1' AS string_field,
+      DATE '2025-03-01' AS date_field,
+      DATETIME '2025-03-01 12:00:00' AS datetime_field,
+      TRUE AS boolean_field ), STRUCT( 2,
+      STRUCT( 'sample2' AS a,
+        FALSE AS b),
+      'Sample String 2',
+      DATE '2025-03-15',
+      DATETIME '2025-03-15 18:30:00',
+      FALSE ) ]);
